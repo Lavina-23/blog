5e2200d003a87337022992fec0ccaa8554eec89c
Lavina-23 <lavinakohai3227@gmail.com>
2025-01-24 13:38:33 +0700

    feat: move post contents to database

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Post extends Model
+{
+    /** @use HasFactory<\Database\Factories\PostFactory> */
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
+
+    // public static function find($slug): array
+    // {
+    //     // function callback
+    //     // return Arr::first(static::all(), function ($post) use ($slug) {
+    //     //     return $post['slug'] == $slug;
+    //     // });
+
+    //     // arrow function
+    //     $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
+
+    //     if (!$post) {
+    //         abort(404);
+    //     }
+
+    //     return $post;
+    // }
+}
