e4fbeeaf009be0abf83c52799fa7a443431c3f0d
Lavina-23 <lavinakohai3227@gmail.com>
2025-01-26 13:07:04 +0700

    feat: eloquent

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,8 @@
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
             <div class="text-gray-500 text-base">
-                <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumans() }}
+                <a class="hover:underline" href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> |
+                {{ $post['created_at']->diffForHumans() }}
             </div>
             <p class="my-4 font-light">
                 {{ Str::limit($post['body'], 300, '...') }}
